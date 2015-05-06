98796c20df4471f232da2081a2c5161d8a274585
Brandon LoGuercio <brandonloguercio@yahoo.com>
2015-05-05 23:12:03 -0700

    May 5th commit

--- ruby-exercise-1.rb
+++ ruby-exercise-1.rb
@@ -1,21 +1,25 @@
 poem = File.read('the-man-from-snowy-river.txt')
 lines = poem.lines
-
-#/\W+/ is a "regular expression" - a text-searching pattern.
-# /\s+/ The slashes start and end the pattern. \W means "any non-alphabetic character"
-# and + means "repeated one or more times"
 words = poem.split /\W+/
+                   #this means split at each non alphabetic character
+                   # /\s+/ means it will split at ever white space
+
+/\W+/ is a "regular expression" - a text-searching pattern.
+/\s+/ The slashes start and end the pattern. \W means "any non-alphabetic character"
+and + means "repeated one or more times"
 
 
-#Example
-puts "There are #{words.count} words"
-puts "There are #{words.uniq.count} different words" #uniq removes duplicates
+# Examples:
+# puts "There are #{words.count} words"
+# puts "There are #{words.uniq.count} different words" #uniq removes duplicates
 
-#Exercises
-puts "The second through sixth words are …"
-puts "The first line has … words in it"
+#Exercises:
 
-first_stanza_lines = 'replace this with code'
-first_stanza = 'replace this with code'
-puts "The first stanza has … words in it"
-puts "Here is the poem with its lines randomly rearranged …"
+puts "The second through sixth words are: #{words[1..5].join ", " }"
+puts "The first line has #{lines[0].split(/\W+/).count } words in it"
+
+first_stanza_lines = lines[0..8]
+first_stanza = first_stanza_lines.join
+
+puts "The first stanza has #{first_stanza.split(/\W+/).count} words in it"
+puts "Here is the poem with its lines randomly rearranged #{lines.shuffle.join}"
